069111e9d747e114095d4eec771c91491c9d28d1
molysama <molysama.i@gmail.com>
2019-10-28 10:47:25 +0800

    update to pro 7.4

--- src/tools/index.js
+++ src/tools/index.js
@@ -1,29 +1,29 @@
-import Core from '@auto.pro/core'
+import Core, {use, cap, scale, getWidth, getHeight, width, height} from '@auto.pro/core'
 
 import Action, {click, swipe} from '@auto.pro/action'
-import Seacth, {findImg, hasAnyColors, hasMulColors, noAnyColors} from '@auto.pro/search'
+import Search, {findImg, hasAnyColors, hasMulColors, noAnyColors} from '@auto.pro/search'
 
-const core = Core({
+Core({
     baseWidth: 1280,
     baseHeight: 720,
     needCap: true
 })
 
-core.use(Action)
-core.use(Seacth)
+use(Action)
+use(Search)
 
 const tap = click
-const scale = core.scale
-const getWidth = core.getWidth
-const getHeight = core.getHeight
-const width = core.width
-const height = core.height
 
 export {
-    click,
+    cap,
+
     tap,
+    click,
     swipe,
+
     findImg,
+    hasAnyColors,
+    hasMulColors,
     scale,
     getWidth,
     getHeight,
