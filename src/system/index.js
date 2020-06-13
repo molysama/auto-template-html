fb0634f4f9724b85070196f7f94d5d9cea80978a
molysama <molysama.i@gmail.com>
2020-06-14 00:15:20 +0800

    添加线上示例

--- src/system/index.js
+++ src/system/index.js
@@ -1,5 +1,5 @@
-import { fromEvent, race, timer } from "rxjs"
-import { share, exhaustMap, tap } from "rxjs/operators"
+import { fromEvent, race, timer, of } from "rxjs"
+import { share, exhaustMap, tap, map } from "rxjs/operators"
 
 import Core, { use } from "@auto.pro/core"
 import WebViewPlugin, { run } from "@auto.pro/webview"
@@ -13,22 +13,36 @@ Core({
 
 use(WebViewPlugin)
 
-const htmlPath = "file:" + files.path("dist/index.html")
+// const htmlPath = "file:" + files.path("dist/index.html")
 // const htmlPath = 'http://www.baidu.com'
+const htmlPath = 'https://core-ui-example-1253839166.cos-website.ap-guangzhou.myqcloud.com/#/dashboard'
 
 export const wv = run(htmlPath)
 
 // 监听返回键
 const back$ = fromEvent(ui.emitter, "back_pressed").pipe(share())
-back$
-    .pipe(
-        exhaustMap((e) => {
-            toast("再次返回可退出")
-            e.consumed = true
-            return race(
-                back$.pipe(tap(() => (e.consumed = false))),
-                timer(2000)
-            )
+back$.pipe(
+    tap(e => {
+        e.consumed = true
+        wv.runHtmlJS('document.location.hash').then(result => {
+            console.log('result', result)
+            if (JSON.parse(result) === '#/dashboard') {
+                exit()
+            } else {
+                wv.runHtmlJS('history.back(-1)')
+            }
         })
-    )
-    .subscribe()
+    })
+).subscribe()
+// back$
+//     .pipe(
+//         exhaustMap((e) => {
+//             toast("再次返回可退出")
+//             e.consumed = true
+//             return race(
+//                 back$.pipe(tap(() => (e.consumed = false))),
+//                 timer(2000)
+//             )
+//         })
+//     )
+//     .subscribe()
